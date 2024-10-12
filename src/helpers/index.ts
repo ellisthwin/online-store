c9a2d8dd47afd51a9680bfad6d252a38904e00b6
Dario Ellis <kyawmgmgthwin12@gmail.com>
2024-10-12 15:52:42 +0700

    adds the new helpers to the template engine configuration

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -2,6 +2,7 @@ import { Express } from "express";
 import { getConfig } from "../config";
 import { engine } from "express-handlebars";
 import * as env_helpers from "./env";
+import * as catalog_helpers from "./catalog_helpers";
 
 const location = getConfig("templates:location"); //location of the templates directory
 const config = getConfig("templates:config"); //other settings related to the Handlebars configuration
@@ -9,7 +10,7 @@ const config = getConfig("templates:config"); //other settings related to the Ha
 export const createTemplates = (app: Express) => {
     app.set("views", location);
     app.engine("handlebars", engine({
-        ...config, helpers: { ...env_helpers }
+        ...config, helpers: { ...env_helpers, ...catalog_helpers }
     }));
     app.set("view engine", "handlebars");
 }
