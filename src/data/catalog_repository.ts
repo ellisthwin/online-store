5e81c3450f9d99a41c71fb43d661bc11d924f6f3
Dario Ellis <kyawmgmgthwin12@gmail.com>
2024-10-12 13:53:12 +0700

    add a parameter in get Products

--- src/data/catalog_repository.ts
+++ src/data/catalog_repository.ts
@@ -1,10 +1,13 @@
-import { Category, Product, Supplier } from "./catalog_models";
+import { Category, Product, Supplier, ProductQueryParameters, ProductQueryResult } from "./catalog_models";
 
 export interface CatalogRepository {
-    getProducts(): Promise<Product[]>;
+    getProducts(params?: ProductQueryParameters): Promise<ProductQueryResult>;   // Retrieves a list of all products. A Promise that resolves to an array of Product objects.
     storeProduct(p: Product): Promise<Product>;
     getCategories() : Promise<Category[]>;
     storeCategory(c: Category): Promise<Category>;
     getSuppliers(): Promise<Supplier[]>;
     storeSupplier(s: Supplier): Promise<Supplier>;
 }
+
+//All methods return Promise objects, indicating that the operations are asynchronous. This is typical when interacting with databases or external APIs.
+//It defines what operations can be performed without dictating how they are implemented. This separation of concerns allows for flexibility in the implementation details.
