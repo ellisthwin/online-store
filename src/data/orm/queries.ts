2e543b1759c6d1c3bb1d8d77bd971e68848aadd9
Dario Ellis <kyawmgmgthwin12@gmail.com>
2024-10-13 14:22:40 +0700

    added search and category queries

--- src/data/orm/queries.ts
+++ src/data/orm/queries.ts
@@ -1,6 +1,7 @@
 import { CategoryModel, ProductModel, SupplierModel } from "./models";
 import { BaseRepo, Constructor} from "./core";
 import { ProductQueryParameters } from "../catalog_models";
+import {Op} from "sequelize";
 
 export function AddQueries<TBase extends Constructor<BaseRepo>>(Base: TBase) {
     return class extends Base {
@@ -11,6 +12,20 @@ export function AddQueries<TBase extends Constructor<BaseRepo>>(Base: TBase) {
                 opts.limit = params?.pageSize,
                 opts.offset = (params.page -1) * params.pageSize
             }
+
+            if(params?.searchTerm){
+                const searchOp = { [Op.like]: "%" + params.searchTerm + "%"};
+                opts.where = { [Op.or]: {name: searchOp, description: searchOp }}
+            } //Products that contain the searchTerm in either their name or description will be included in the results.
+
+            if (params?.category) {
+                opts.where =  {
+                    ...opts.where, categoryId: params.category //Merges the existing filter conditions (opts.where) with the new condition categoryId: params.category.
+                }
+            } //Only products that belong to the specified category are included in the results.
+            
+            const categories = await this.getCategories();
+
             const result = await ProductModel.findAndCountAll({
                 include: [
                     {model: SupplierModel, as: "supplier"},
@@ -19,7 +34,8 @@ export function AddQueries<TBase extends Constructor<BaseRepo>>(Base: TBase) {
                 raw: true, nest: true,
                 ...opts
             });
-            return { products: result.rows, totalCount: result.count};
+
+            return { products: result.rows, totalCount: result.count, categories };
         }
 
         getCategories() {
