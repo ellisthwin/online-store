f1dd5a43a9bc2ad3552ec309730814d0da8a6289
Dario Ellis <kyawmgmgthwin12@gmail.com>
2024-10-13 16:52:45 +0700

    add helper function for user control buttons

--- src/helpers/catalog_helpers.ts
+++ src/helpers/catalog_helpers.ts
@@ -7,8 +7,8 @@ const getData = (options:HelperOptions) => {
 };
 
 export const navigationUrl = (options: HelperOptions) => {
-    const { page, pageSize } = getData(options);
-    return "/?" + stringify({ page, pageSize });
+    const { page, pageSize, category, searchTerm } = getData(options);
+    return "/?" + stringify({ page, pageSize, category, searchTerm });
 }
 
 export const escapeUrl = (url: string) => escape(url);
@@ -31,5 +31,19 @@ export const pageSizeOptions = (options:HelperOptions) => {
     [3,6,9].forEach(size => {
         output += options.fn({ size, selected: pageSize === size ? "selected": ""})
     })
+    return output;
+}
+
+export const categoryButtons = (options: HelperOptions) => {
+    const { category, categories } = getData(options);
+    let output = "";
+    for (let i=0; i < categories.length; i++)
+    {
+        output += options.fn({
+            id: categories[i].id,
+            name: categories[i].name,
+            selected: category === categories[i].id
+        })
+    }
     return output;
 }
