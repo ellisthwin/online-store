50f7b55e9e94f07a7b8fb2e809cb27f368a5d09b
Dario Ellis <kyawmgmgthwin12@gmail.com>
2024-10-18 23:11:57 +0700

    add orderqueries and storeage queries to CatalogRepoIml

--- src/data/orm/models/index.ts
+++ src/data/orm/models/index.ts
@@ -1,8 +1,12 @@
 import { Sequelize } from "sequelize";
 import { initializeCatalogModels } from "./catalog_helpers";
+import { initializeCustomerModels} from "./customer_helpers";
+import { initializeOrderModels } from "./order_helpers";
 
 export { ProductModel, CategoryModel, SupplierModel} from "./catalog_models"; //any module that imports from this script can also access these three models without directly importing from "./catalog_models".
 
 export const initializeModels = (sequelize: Sequelize) => {  //This function can be expanded to include initialization of additional models or configurations
     initializeCatalogModels(sequelize); // by linking these models with the provided Sequelize instance, we can now use them to interact with the database.
+    initializeCustomerModels(sequelize);
+    initializeOrderModels(sequelize);
 }
